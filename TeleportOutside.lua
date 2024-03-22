7eaa32407b6a8f10666163be004634961cc45f19
sheumais <88064574+sheumais@users.noreply.github.com>
2024-03-23 00:02:58 +1100

    command to list houseIds

--- TeleportOutside.lua
+++ TeleportOutside.lua
@@ -12,10 +12,21 @@ local function teleportOutsideHouse(houseId)
     RequestJumpToHouse(houseId, true)
 end
 
+local function printHouseIdList()
+    for houseId = 1, 150 do
+        local houseCollectibleId = GetCollectibleIdForHouse(houseId)
+        if houseCollectibleId ~= 0 then
+            local houseName = GetCollectibleName(houseCollectibleId)
+            d(houseId .. " - " .. houseName)
+        end
+    end
+end
+
 local function Init(event, name)
     if name ~= TeleportOutsideNamespace.name then return end
     EVENT_MANAGER:UnregisterForEvent(TeleportOutsideNamespace.name, EVENT_ADD_ON_LOADED)
     SLASH_COMMANDS["/tpo"] = teleportOutsideHouse
+    SLASH_COMMANDS["/tpolist"] = printHouseIdList
 end
 
 EVENT_MANAGER:RegisterForEvent(TeleportOutsideNamespace.name, EVENT_ADD_ON_LOADED, Init)
