fb5d952a044150d441f8fba5cd69c4c9b62c6de1
sheumais <88064574+sheumais@users.noreply.github.com>
2024-03-23 17:55:11 +1100

    add string matching

--- TeleportOutside.lua
+++ TeleportOutside.lua
@@ -4,20 +4,31 @@ TeleportOutsideNamespace = {
     author = "TheMrPancake",
 }
 
-local function teleportOutsideHouse(houseId)
-    if houseId == "" then
+local houseList = {}
+
+local function teleportOutsideHouse(houseInput)
+    if houseInput == "" then
         d("You need to specify a houseId")
         return
     end
-    RequestJumpToHouse(houseId, true)
+    if tonumber(houseInput) then
+        RequestJumpToHouse(houseInput, true)
+        return
+    elseif type(houseInput) == "string" then
+        for houseId, houseName in pairs(houseList) do
+            if houseName:lower():find(houseInput:lower()) then
+                RequestJumpToHouse(houseId, true)
+                return end
+        end
+    end
 end
 
-local function printHouseIdList()
-    for houseId = 1, 150 do
+local function createHouseIdList()
+    for houseId = 1, 115 do
         local houseCollectibleId = GetCollectibleIdForHouse(houseId)
         if houseCollectibleId ~= 0 then
             local houseName = GetCollectibleName(houseCollectibleId)
-            d(houseId .. " - " .. houseName)
+            houseList[houseId] = houseName
         end
     end
 end
@@ -25,8 +36,8 @@ end
 local function Init(event, name)
     if name ~= TeleportOutsideNamespace.name then return end
     EVENT_MANAGER:UnregisterForEvent(TeleportOutsideNamespace.name, EVENT_ADD_ON_LOADED)
+    createHouseIdList()
     SLASH_COMMANDS["/tpo"] = teleportOutsideHouse
-    SLASH_COMMANDS["/tpolist"] = printHouseIdList
 end
 
 EVENT_MANAGER:RegisterForEvent(TeleportOutsideNamespace.name, EVENT_ADD_ON_LOADED, Init)
